1253e799e95f11fba2dd0d266e11bbe0a15df1b5
gabrielVrodriguez <121834753+gabrielVrodriguez@users.noreply.github.com>
2026-01-25 22:58:25 -0300

    feat: implement Prisma-based gym repository and define DTO for gym creation.

--- src/modules/gym/dtos/create-gym.ts
+++ src/modules/gym/dtos/create-gym.ts
@@ -2,12 +2,12 @@
 import { z } from "zod"
 
 export const createGymDto = z.object({
-    id: z.string().optional(),
-    title: z.string().min(3),
-    description: z.string().optional().nullable(),
-    phone: z.string().optional().nullable(),
-    latitude: z.number(),
-    longitude: z.number(),
-  })
+  id: z.string(), 
+  title: z.string().min(3),
+  description: z.string().optional().nullable(),
+  phone: z.string().optional().nullable(),
+  latitude: z.number(),
+  longitude: z.number(),
+})
 
 export type CreateGymSchema = z.infer<typeof createGymDto>
--- src/modules/gym/repositories/prisma-gym-repository.ts/prisma-gym-repository.ts
+++ src/modules/gym/repositories/prisma-gym-repository.ts/prisma-gym-repository.ts
@@ -13,24 +13,47 @@ export class PrismaGymRepository implements IGymRepository {
 
         if (!gym) return null
 
-        return {
-            ...gym,
-            longitude: Number(gym.longitude),
-            latitude: Number(gym.latitude)
-        }
+        return gym
     }
+
     async create(data: CreateGymSchema): Promise<Gym> {
         const gym = await prisma.gym.create({
-            data
+            data: {
+                title: data.title,
+                description: data.description ?? null,
+                phone: data.phone ?? null,
+                latitude: data.latitude,
+                longitude: data.longitude,
+                id: data.id,
+            }
         })
 
         return gym
     }
+
     async searchMany(query: string, page: number): Promise<Gym[]> {
-        throw new Error("Method not implemented.");
+        const gyms = await prisma.gym.findMany({
+            where: {
+                title: {
+                    contains: query
+                },
+            },
+            take: 20,
+            skip: (page - 1) * 20,
+        })
+
+        return gyms
     }
-    async fetchNearbyGyms(params: fetchNearbyGymsSchema): Promise<Gym[]> {
-        throw new Error("Method not implemented.");
+
+    async fetchNearbyGyms({ latitude, longitude }: fetchNearbyGymsSchema): Promise<Gym[]> {
+        const gyms = await prisma.$queryRaw<Gym[]>`
+        SELECT * from gyms
+        WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude )
+         - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+        `
+
+        return gyms
     }
+    
 
 }
